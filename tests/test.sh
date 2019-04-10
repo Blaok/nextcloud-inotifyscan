01199ed8452fa54007985b42d5d55270a7ba6b30
Blaok <i@blaok.me>
2019-04-09 17:04:05 -0700

    test: include testcases for utf-8 and python3

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,10 @@ inotifyscan="${testdir}/../nextcloud-inotifyscan"
 nextcloud="${mockdir}/nextcloud"
 datadir="${mockdir}/data"
 tmp=$(mktemp)
+python2=/usr/bin/python
+python3=/usr/bin/python
+test -x /usr/bin/python2 && python2=/usr/bin/python2
+test -x /usr/bin/python3 && python3=/usr/bin/python3
 
 PATH="${mockdir}/bin:${PATH}"
 export INTERVAL=0.1
@@ -20,6 +24,10 @@ function run-for-bob() {
   sleep .2
   echo foo > "${nextcloud}/data/bob/files/bar"
   sleep .2
+  echo foo > "${nextcloud}/data/bob/files/한국어"
+  sleep .2
+  rm "${nextcloud}/data/bob/files/한국어"
+  sleep .2
   rm "${nextcloud}/data/bob/files/bar"
   sleep .2
 }
@@ -30,18 +38,24 @@ function run-for-alice() {
   sleep .2
   rm "${datadir}/alice/files/bar"
   sleep .2
+  echo foo > "${datadir}/alice/files/조선말"
+  sleep .2
+  rm "${datadir}/alice/files/조선말"
+  sleep .2
 }
 # Use case 1:
 #   set NEXTCLOUD_HOME and USER_NAME
 #   php must be available in $PATH
 #   $NEXTCLOUD_HOME/occ will be used
 #   data directory is under $NEXTCLOUD_HOME/data
-NEXTCLOUD_HOME="${nextcloud}" USER_NAME=bob "${inotifyscan}" >${tmp} &
+NEXTCLOUD_HOME="${nextcloud}" USER_NAME=bob ${python3} "${inotifyscan}" >${tmp} &
 child=$!
 run-for-bob
 kill-all ${child}
 diff <(cat <<EOF
 php ${nextcloud}/occ files:scan --no-interaction --path=/bob/files/bar --shallow
+php ${nextcloud}/occ files:scan --no-interaction --path=/bob/files/한국어 --shallow
+php ${nextcloud}/occ files:scan --no-interaction --path=/bob/files/ --shallow
 php ${nextcloud}/occ files:scan --no-interaction --path=/bob/files/ --shallow
 EOF
 ) ${tmp} || exit 1
@@ -51,12 +65,14 @@ EOF
 #   php must be available in $PATH
 #   $NEXTCLOUD_HOME/occ will be used
 #   data directory is customized
-NEXTCLOUD_HOME="${nextcloud}" USER_NAME=alice DATA_DIR="${datadir}" "${inotifyscan}" >${tmp} &
+NEXTCLOUD_HOME="${nextcloud}" USER_NAME=alice DATA_DIR="${datadir}" ${python2} "${inotifyscan}" >${tmp} &
 child=$!
 run-for-alice
 kill-all ${child}
 diff <(cat <<EOF
 php ${nextcloud}/occ files:scan --no-interaction --path=/alice/files/bar --shallow
+php ${nextcloud}/occ files:scan --no-interaction --path=/alice/files/ --shallow
+php ${nextcloud}/occ files:scan --no-interaction --path=/alice/files/조선말 --shallow
 php ${nextcloud}/occ files:scan --no-interaction --path=/alice/files/ --shallow
 EOF
 ) ${tmp} || exit 2
@@ -73,6 +89,8 @@ run-for-bob
 kill-all ${child}
 diff <(cat <<EOF
 docker exec -uuserfoo containerbar php occ files:scan --no-interaction --path=/bob/files/bar --shallow
+docker exec -uuserfoo containerbar php occ files:scan --no-interaction --path=/bob/files/한국어 --shallow
+docker exec -uuserfoo containerbar php occ files:scan --no-interaction --path=/bob/files/ --shallow
 docker exec -uuserfoo containerbar php occ files:scan --no-interaction --path=/bob/files/ --shallow
 EOF
 ) ${tmp} || exit 3
@@ -89,6 +107,8 @@ run-for-alice
 kill-all ${child}
 diff <(cat <<EOF
 docker exec -uuserfoo containerbar php occ files:scan --no-interaction --path=/alice/files/bar --shallow
+docker exec -uuserfoo containerbar php occ files:scan --no-interaction --path=/alice/files/ --shallow
+docker exec -uuserfoo containerbar php occ files:scan --no-interaction --path=/alice/files/조선말 --shallow
 docker exec -uuserfoo containerbar php occ files:scan --no-interaction --path=/alice/files/ --shallow
 EOF
 ) ${tmp} || exit 4
